87c252375d00d2de434e3628845ae13ac3812219
Harinony <harinonyaurelien@gmail.com>
2023-11-21 10:41:36 +0300

    Initial commit

---
+++ apps/storybook-mobile/jest.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  displayName: 'storybook-mobile',
+  preset: 'react-native',
+  resolver: '@nx/jest/plugins/resolver',
+  moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+  moduleNameMapper: {
+    '\\.svg$': '@nx/react-native/plugins/jest/svg-mock',
+  },
+  coverageDirectory: '../../coverage/apps/storybook-mobile',
+};
